5445e3fdd05206ce509bea00e405daed3c757e57
Alex Yermakov <gates_b@bigmir.net>
2017-11-08 18:52:56 +0200

    Added native draft promises and async/await versions

--- MainController.js
+++ MainController.js
@@ -1,6 +1,7 @@
 const
     coffeeService = require('./coffeeService'),
-    async = require('async');
+    async = require('async'),
+    _ = require('lodash');
 
 
 class MainController {
@@ -49,15 +50,72 @@ class MainController {
     }
 
     nativePromiseVersion(req, res) {
-        res.json({ nothing: true });
-    }
+        let result = {};
 
-    asyncAwaitVersion(req, res) {
-        res.json({ nothing: true });
+        coffeeService.start();
+
+        let getWater = coffeeService.get('getWater')
+            .then((getWater) => _.extend(result, { getWater }));
+
+        let frameMug = coffeeService.get('frameMug')
+            .then((frameMug) => _.extend(result, { frameMug }));
+
+        let processCoffee = coffeeService.get('getCoffee')
+            .then((getCoffee) => _.extend(result, { getCoffee }))
+            .then(() => coffeeService.get('grindCoffee'))
+            .then((grindCoffee) => _.extend(result, { grindCoffee }));
+
+        let processMilk = coffeeService.get('getMilk')
+            .then((getMilk) => _.extend(result, { getMilk }))
+            .then(() => coffeeService.get('boilMilk'))
+            .then((boilMilk) => _.extend(result, { boilMilk }));
+
+        let pourMilk = Promise.all([ processMilk, frameMug])
+            .then(() => coffeeService.get('pourMilkIntoMug'))
+            .then((pourMilkIntoMug) => _.extend(result, { pourMilkIntoMug }));
+
+        Promise.all([ getWater, processCoffee, pourMilk ])
+            .then((data) => _.extend(result, data))
+            .then(() => coffeeService.get('coffeeReadyToGo'))
+            .then((makeCoffee) => _.extend(result, { makeCoffee }))
+            .then(() => res.json({ time: coffeeService.finish(result) }));
     }
 
     rxJSVersion(req, res) {
         res.json({ nothing: true });
+    }
+
+    async asyncAwaitVersion(req, res) {
+        let result = {};
+
+        coffeeService.start();
+
+        let getWater = async () =>
+            result.getWater = await coffeeService.get('getWater');
+
+        let frameMug = async () =>
+            result.frameMug = await coffeeService.get('frameMug');
+
+        let processCoffee = async () => {
+            result.getCoffee = await coffeeService.get('getCoffee');
+            result.grindCoffee = await coffeeService.get('grindCoffee');
+        };
+
+        let processMilk = async () => {
+            result.getMilk = await coffeeService.get('getMilk');
+            result.boilMilk = await coffeeService.get('boilMilk');
+        };
+
+        let pourMilk = async () => {
+            await Promise.all([ processMilk(), frameMug() ]);
+            result.pourMilkIntoMug = await coffeeService.get('pourMilkIntoMug');
+        };
+
+        await Promise.all([ getWater(), processCoffee(), pourMilk() ]);
+
+        result.makeCoffee = await coffeeService.get('coffeeReadyToGo');
+
+        res.json({ time: coffeeService.finish(result) });
     }
 
 }
